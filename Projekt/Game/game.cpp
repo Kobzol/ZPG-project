1c62cd000f3f0d6a140d676a157a1186ba94a18b
Jakub Beránek <berykubik@gmail.com>
2015-10-13 11:56:55 +0200

    ENH: removed unincluded shaders and renamed test shaders to lambert shader

--- Projekt/Game/game.cpp
+++ Projekt/Game/game.cpp
@@ -1,4 +1,5 @@
 #include "game.h"
+#include "models/suzi_flat.h"
 
 Game Game::instance = Game();
 
@@ -30,15 +31,10 @@ void Game::start()
 	this->context->setMousePositionCallback([](GLFWwindow* window, double x, double y) { Game::getInstance().onMouseMoveCallback(window, x, y); });
 	this->context->setMouseScrollCallback([](GLFWwindow* window, double xOffset, double yOffset) { Game::getInstance().onMouseScrollCallback(window, xOffset, yOffset); });
 	this->context->setMouseButtonCallback([](GLFWwindow* window, int button, int action, int modifier) { Game::getInstance().onMouseButtonCallback(window, button, action, modifier); });
+	this->context->setDepthTest(true);
 
-	Shader vertShader(FileHelper::loadFile("Shaders/Vertex/test.vert"), GL_VERTEX_SHADER);
-	Shader fragShader(FileHelper::loadFile("Shaders/Fragment/test.frag"), GL_FRAGMENT_SHADER);
-
-	float points[] = {
-		0.0f, 0.0f, 0.0f,	// upper center
-		0.5f, -0.5f, 0.0f,	// lower right
-		-0.5f, -0.5f, 0.0f,	// lower left
-	};
+	Shader vertShader(FileHelper::loadFile("Shaders/Vertex/lambert.vert"), GL_VERTEX_SHADER);
+	Shader fragShader(FileHelper::loadFile("Shaders/Fragment/lambert.frag"), GL_FRAGMENT_SHADER);
 
 	Program program;
 	program.attachShader(vertShader);
@@ -47,11 +43,11 @@ void Game::start()
 	program.use();
 
 	VAO vao;
-	vao.bind();	
+	vao.bind();
 
 	VBO vbo;
 	vbo.bind();
-	vbo.setData(points, sizeof(points), GL_STATIC_DRAW);
+	vbo.setData(VERTICES, sizeof(VERTICES), GL_STATIC_DRAW);
 
 	double xpos_old = -1;
 	double ypos_old = -1;
@@ -67,6 +63,7 @@ void Game::start()
 	glm::mat4 model;
 
 	program.setAttribute("position", 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, position));
+	program.setAttribute("normal", 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, normal));
 
 	Camera camera(glm::vec3(0.0f, 0.0f, 0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), 45.0f, 4.0f / 3.0f, 0.1f, 10.0f);
 	camera.attachListener(&program);
@@ -84,10 +81,11 @@ void Game::start()
 
 		vao.bind();
 
+		model = glm::rotate(model, glm::radians(10.0f * context.getDeltaTime()), glm::vec3(0.0f, 0.0f, 1.0f));
 		program.setUniformMatrix4fv("Model", model);
-		program.setUniform3f("color", glm::vec3(1.0f, 0.0f, 0.0f));
+		program.setUniform4f("color", glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
 
-		this->renderer.drawTriangles(0, 3);
+		this->renderer.drawTriangles(0, pocetPrvku);
 
 		double diffX = this->mousePosition.first - this->oldMousePosition.first;
 		double diffY = this->oldMousePosition.second - this->mousePosition.second;
--- Projekt/Shaders/Fragment/lambert.frag
+++ Projekt/Shaders/Fragment/lambert.frag
@@ -16,7 +16,7 @@ void main()
 	vec4 lightPosition = vec4(10.0f, 10.0f, 10.0f, 1.0f);
 	vec4 ambient = vec4(0.1f, 0.1f, 0.1f, 1.0f);
 
-	vec3 lightDir = vec3(normalize(lightPosition - outVertex.worldPosition));
+	vec3 lightDir = vec3(normalize(lightPosition));
 	float dotLight = max(dot(lightDir, normalize(outVertex.outNormal)), 0.0f);
 	
 	vec4 diffColor = dotLight * color;
