b4b70b8eb2584fb4eecf96f7439e5ebfe7a0c8c7
Jakub Beránek <berykubik@gmail.com>
2015-10-16 21:21:20 +0200

    ENH: commented crash-causing VBO deletion

--- Projekt/Context/context.cpp
+++ Projekt/Context/context.cpp
@@ -36,7 +36,7 @@ GLboolean Context::getGLBoolean(bool value)
 void Context::initialize()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
