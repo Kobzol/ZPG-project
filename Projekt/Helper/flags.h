bb62bf084b8f12917edabdf04ce7d91696d82ecb
Jakub Beránek <berykubik@gmail.com>
2015-10-16 09:23:16 +0200

    ENH: added multiple flag operations to flags

--- Projekt/Helper/flags.h
+++ Projekt/Helper/flags.h
@@ -21,13 +21,39 @@ public:
 	{
 		this->value.set((int) enumValue);
 	}
+	void set(std::initializer_list<T> enumValues)
+	{
+		for (T t : enumValues)
+		{
+			this->set(t);
+		}
+	}
 	void unset(T enumValue)
 	{
 		this->value.reset((int) enumValue);
 	}
+	void unset(std::initializer_list<T> enumValues)
+	{
+		for (T t : enumValues)
+		{
+			this->unset(t);
+		}
+	}
 	bool isSet(T enumValue)
 	{
 		return this->value.test((int) enumValue);
+	}
+	bool isSet(std::initializer_list<T> enumValues)
+	{
+		for (T t : enumValues)
+		{
+			if (!this->isSet(t))
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 	void reset()
 	{
