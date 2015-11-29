dde262e3eb6f0e42747bfd744b2c5dc27366ce06
Jakub Beránek <berykubik@gmail.com>
2015-11-29 21:18:50 +0100

    ENH: added check for badly loaded image

--- Projekt/Helper/image.cpp
+++ Projekt/Helper/image.cpp
@@ -4,6 +4,11 @@ Image::Image(std::string path, ImageType type) : type(type)
 {
 	int channels;
 	this->data = SOIL_load_image(path.c_str(), &width, &height, &channels, (type == ImageType::Alpha) ? SOIL_LOAD_RGBA : SOIL_LOAD_RGB);
+
+	if (this->data == nullptr)
+	{
+		throw std::runtime_error("Couldn't load image: " + path);
+	}
 }
 Image::Image(int width, int height, ImageType type) : width(width), height(height), data(nullptr), type(type)
 {
